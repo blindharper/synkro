525c302f11db8db749b8eba742d5428ca8f1777f
blindharper <blindharper70@gmail.com>
2025-11-14 16:12:52 +0700

    Solutions and projects migrated to Visual Studio 2026 format.

--- synkro.tests/src/anim/Test_Expression.cpp
+++ synkro.tests/src/anim/Test_Expression.cpp
@@ -27,11 +27,11 @@ public:
 		ExpressionCompiler compiler( nullptr );
 
 		ExpressionScript script( &compiler, L"return time;" );
-		script.DeclareParam( L"time", Type::Float );
+		script.DeclareParam( L"time", AnimationDataType::Float );
 		ExpressionParam* paramTime = script.GetParam( L"time" );
 		script.Prepare();
 		script.SetParamValue( paramTime, 3.14f );
-		Assert( script.GetType() == Type::Float );
+		Assert( script.GetType() == AnimationDataType::Float );
 
 		Float value;
 		script.GetValue( value );
@@ -41,17 +41,17 @@ public:
 		ExpressionScript script2( &compiler, text );
 		script2.Prepare();
 		Float f; script2.GetValue( f );
-		Assert( script2.GetType() == Type::Float );
+		Assert( script2.GetType() == AnimationDataType::Float );
 		Assert( f == 0.5f );
 
 		/*String text3(L"float r = 0.5; float g = sin( 2.0*PI*time ); float b = 2.0*cos( 2.0*PI*time ) - 0.7; color c(0.1, 0.2, 0.3); return color(r+c.r, g+c.g, b+c.b);");
 		ExpressionScript script3( &compiler, text3 );
-		script2.DeclareParam( L"time", Type::Float );
+		script2.DeclareParam( L"time", AnimationDataType::Float );
 		ExpressionParam* paramTime3 = script3.GetParam( L"time" );
 		script3.Prepare();
 		script3.SetParamValue( paramTime3, 0.0f );
 		Color color; script3.GetValue( color );
-		Assert( script3.GetType() == Type::Color );*/
+		Assert( script3.GetType() == AnimationDataType::Color );*/
 	}
 
 } _tst;
